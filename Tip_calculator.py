141fcaebfac021f454c2e78c5a1ef11d867f29ac
Albert Anati <anatiprogrammer@gmail.com>
2023-02-21 22:33:03 +0000

    tip calculator

---
+++ Tip_calculator.py
@@ -0,0 +1,29 @@
+# Tip calculator
+#If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+print('Welcome to Anati tip Calculator') # Welcome message
+
+total_bill = input('What was the total bill? $')
+
+tip_percent = input('What percentage tip would you like to give? 10, 12 or 15? ')
+
+people = input('How many people to split the bill? ')
+
+n_totalbill = float(total_bill)
+
+n_tipercent = float(tip_percent) / 100
+
+n_people = int(people)
+
+each_pay = ((n_totalbill * n_tipercent) + n_totalbill) / n_people
+
+r_eachpay = '{:.2f}'.format(each_pay)
+
+print(f'Each person should pay: ${r_eachpay}')
